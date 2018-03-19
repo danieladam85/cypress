c9565c870652ac5d174ff3bd563caaf1e51730ac
Daniel <buda2bala@gmail.com>
2018-03-19 19:30:28 +0100

    added extra step

--- cypress/integration/input-form-spec.js
+++ cypress/integration/input-form-spec.js
@@ -1,9 +1,23 @@
 describe('Input-form', () => {
-    it('opens the Rabobank website', ()  => {
+    it('opens the Rabobank website', () => {
         cy.visit('http://rabobank.nl')
+
+    })
+    it('searches for test', () => {
         cy.get('#ra_searchfield')
             .type("test")
-            .type({enter})
+            .type("{enter}")
+        cy.get('#cookiewall_popup .primary-button')
+            .click()
+    })
+    it('searches for adviesplanner', () => {
+        cy.get('#cookiewall_popup .primary-button')
+            .click()
+        cy.get('#zoek')
+            .clear()
+            .type("adviesplanner")
+            .type("{enter}")
 
+    })
 
 })
